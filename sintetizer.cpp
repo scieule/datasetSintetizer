2b4eb1078686492bd94d497ef2ac261639deeb75
Rafael Henrique Tibães <r@fael.nl>
2017-06-01 15:29:04 -0300

    crop

--- sintetizer.cpp
+++ sintetizer.cpp
@@ -5,7 +5,7 @@
  * @Project: Biometrics Research
  * @Filename: sintetizer.cpp
  * @Last modified by:   rafael
- * @Last modified time: 2017-06-01T12:52:40-03:00
+ * @Last modified time: 2017-06-01T15:28:00-03:00
  * @License: MIT
  * @Copyright: Nandlands and Akiyama
  */
@@ -23,16 +23,19 @@ using namespace std;
 using namespace cv;
 
 int main(int argc, char **argv) {
-  const auto keys =
-      "{help h usage ? | | Display this message}"
-      "{input i in | | input image}"
-      "{output o out | | output image}"
-      "{scale s | | scale factor in %}"
-      "{rotate90 r90 | | rotate in 90 degrees}"
-      "{rotate180 r180 | | rotate in 180 degrees}"
-      "{rotate270 r270 | | rotate in 270 degrees}"
-      "{crop c | | size of the computed crops in % relative to input}"
-      "{nocl | | set to not use OpenCL}";
+  const auto keys = "{help h usage ? | | Display this message}"
+                    "{input i in | | input image}"
+                    "{output o out | | output image}"
+                    "{scale s | | scale factor in %}"
+                    "{rotate90 r90 | | rotate in 90 degrees}"
+                    "{rotate180 r180 | | rotate in 180 degrees}"
+                    "{rotate270 r270 | | rotate in 270 degrees}"
+                    "{crop1 c1 | | crop the first quadrant}"
+                    "{crop2 c2 | | crop the second quadrant}"
+                    "{crop3 c3 | | crop the third quadrant}"
+                    "{crop4 c4 | | crop the fourth quadrant}"
+                    "{crop0 c0 | | crop the center with half the size}"
+                    "{nocl | | set to not use OpenCL}";
   auto cmd = CommandLineParser(argc, argv, keys);
 
   if (cmd.has("help") || !(cmd.has("input") && cmd.has("output"))) {
@@ -45,6 +48,21 @@ int main(int argc, char **argv) {
   UMat input, output;
   imread(cmd.get<string>("input"), IMREAD_UNCHANGED).copyTo(input);
   input.copyTo(output);
+
+  const auto halfHeight = input.size().height / 2;
+  const auto halfWidth = input.size().width / 2;
+  if (cmd.has("crop1"))
+    input(Rect(0, 0, halfWidth, halfHeight)).copyTo(output); // first quadrant
+  else if (cmd.has("crop2"))
+    input(Rect(halfWidth, 0, halfWidth, halfHeight)).copyTo(output);
+  else if (cmd.has("crop3"))
+    input(Rect(halfWidth, halfHeight, halfWidth, halfHeight)).copyTo(output);
+  else if (cmd.has("crop4"))
+    input(Rect(0, halfHeight, halfWidth, halfHeight)).copyTo(output);
+  else if (cmd.has("crop0"))
+    input(Rect(halfWidth / 2, halfHeight / 2, halfWidth, halfHeight))
+        .copyTo(output);
+  output.copyTo(input);
 
   if (cmd.has("scale")) {
     double s = cmd.get<double>("scale");
